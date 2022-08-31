2ac57946088034cd001586c976a1fbc77b4ce113
Stas Marchenko <stas6100@gmail.com>
2022-08-31 14:16:54 +0200

    add sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-
     'storybook-addon-material-ui',
+    'storybook-addon-designs',
   ],
   framework: '@storybook/react',
   core: {
